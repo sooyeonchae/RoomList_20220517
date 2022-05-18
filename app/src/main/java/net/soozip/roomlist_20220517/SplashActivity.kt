7a305a4b0a7ed96f91a69141ccc929d94e7dad36
채수연 <sooyeonchae@gmail.com>
2022-05-18 09:41:11 +0900

    activity별 테마 설정 가능.

---
+++ app/src/main/java/net/soozip/roomlist_20220517/SplashActivity.kt
@@ -0,0 +1,11 @@
+package net.soozip.roomlist_20220517
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
