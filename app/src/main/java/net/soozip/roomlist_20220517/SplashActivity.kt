a82c2edd318094cd8e3f2ac1a4cb5416a8c6a500
채수연 <sooyeonchae@gmail.com>
2022-05-18 09:50:28 +0900

    2초 지연처리할때 handler를 사용함.

--- app/src/main/java/net/soozip/roomlist_20220517/SplashActivity.kt
+++ app/src/main/java/net/soozip/roomlist_20220517/SplashActivity.kt
@@ -3,6 +3,8 @@ package net.soozip.roomlist_20220517
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,12 +12,15 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         //2초 정도 후에 (지연 시킨 후에) 화면 이동
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish() //새 화면 띄우고 현 화면 종료.
+        }, 2000)
 
 
-
-        val myIntent = Intent(this, MainActivity::class.java)
-        startActivity(myIntent)
-
-        finish() //새 화면 띄우고 현 화면 종료.
     }
 }
